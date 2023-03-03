6599ea601c650e418be3f679bb560ab761e2d7b9
tupakkatapa <jeskar95@outlook.com>
2023-03-03 15:46:52 +0200

    Added verbose flag + refactor

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@
 
 # Define variables
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+NIXPKGS_REPO="https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-unstable.zip"
+HM_REPO="https://github.com/nix-community/home-manager/archive/master.tar.gz"
+config_dir="$SCRIPT_DIR/configs/nix_configs/hosts"
 output_dir="$SCRIPT_DIR/result"
+verbose=false
 keep_configs=false
 prompt=false
 
@@ -18,16 +22,44 @@ help() {
     echo "-o, --output          Specify output directory (default: './result')"
     echo ""
     echo "If config_file is not specified, the script will read from standard input."
-    echo "Config files should be in YAML or JSON format, and should formatted correctly."
+    echo "Config files should be in YAML or JSON format and be formatted correctly."
     exit 0
+}
+
+# Handles messages and prompts
+say() {
+    local message="$1"
+
+    # [-] Check for error message
+    if [[ "$message" =~ \[\-\] ]]; then
+        echo -e "$message" >&2
+        exit 1
+
+    # [?] Prompt for user input
+    elif [[ "$message" =~ \[\?\] ]]; then
+        if [ "$prompt" == true ]; then
+            echo ""
+            read -r -p "$message" choice
+        else
+            choice="y"
+        fi
+        if [ "$choice" != "y" ]; then
+            return 1
+        else
+            return 0
+        fi
+
+    # [+] Print message if verbose mode is on
+    elif [ "$verbose" == true ]; then
+        echo -e "$message"
+    fi
 }
 
 # Check main dependencies (nix, python, jinja2)
 check_deps() {
     # Check if nix-build is available
     if ! command -v nix-build >/dev/null 2>&1; then
-        echo "[-] Nix package manager is not installed."
-        exit 1
+        say "[-] Nix package manager is not installed."
     fi
 
     # Check if python is installed
@@ -36,14 +68,12 @@ check_deps() {
     elif command -v python >/dev/null 2>&1 ; then
         python_cmd="python"
     else
-        echo "[-] Python is not installed."
-        exit 1
+        say "[-] Python is not installed."
     fi
 
     # Check that python can import jinja2
     if ! $python_cmd -c "import jinja2" >/dev/null 2>&1; then
-        echo "[-] Jinja2 is not installed."
-        exit 1
+        say "[-] Jinja2 is not installed."
     fi
 }
 
@@ -51,16 +81,17 @@ check_deps() {
 parse_args() {
     while [[ "$#" -gt 0 ]]; do
         case $1 in
-            -p|--prompt) prompt=true;;
+            -p|--prompt) prompt=true ;;
             -k|--keep-configs) keep_configs=true ;;
             -o|--output) output_dir="$2"; shift ;;
+            -h|--help) help ;;
+            -v|--verbose) verbose=true ;;
             -h|--help) help;;
             *.yml|*.yaml|*.json)
                 config_file="$1"
                 ;;
-            *) echo "[-] Unrecognized option '$1'" >&2
-            echo "Try './build.sh --help' for more information."
-            exit 1 ;;
+            *) say "[-] Unrecognized option '$1'"
+            ;;
         esac
         shift
     done
@@ -91,8 +122,7 @@ get_filetype() {
         elif [[ "$firstline" == "{"* ]]; then
             filetype="json"
         else
-            echo "[-] Unable to detect the data format as YAML or JSON."
-            exit 1
+            say "[-] Unable to detect the data format as YAML or JSON."
         fi
     fi
 }
@@ -106,8 +136,7 @@ get_hostnames() {
         yaml|yml)
             # Check if yq is installed
             if ! command -v yq >/dev/null 2>&1; then
-                echo "[-] yq is not installed."
-                exit 1
+                say "[-] yq is not installed."
             fi
             # Extract the names of the hosts from the YAML file
             hostnames=$(yq -r '.hosts[].name' "$config_file")
@@ -116,22 +145,19 @@ get_hostnames() {
 
             # Check if jq is installed
             if ! command -v jq >/dev/null 2>&1; then
-                echo "[-] jq is not installed."
-                exit 1
+                say "[-] jq is not installed."
             fi
             # Extract the host names from the JSON file
             hostnames=$(jq -r '.hosts[].name' "$config_file")
             ;;
         *)
-            echo "[-] Invalid data format. Only YAML and JSON are supported."
-            exit 1
+            say "[-] Invalid data format. Only YAML and JSON are supported."
             ;;
     esac
 
     # Check if hosts are empty
     if [ -z "$hostnames" ]; then
-        echo "[-] No hosts found in $config_file"
-        exit 1
+        say "[-] No hosts found in $config_file"
     fi
 }
 
@@ -139,14 +165,10 @@ get_hostnames() {
 check_prev_config() {
     local dir=$1
     if [ -d "$dir" ] && [ "$(ls -A "$dir")" ]; then
-        if [ "$prompt" == true ]; then
-            read -r -p "[?] Delete previous config files and render again? (y/n)" choice
-        else
-            choice="y"
-        fi
-        if [ "$choice" != "y" ]; then
-            echo "[+] Exiting..."
-            exit 1
+        if ! say "[?] Delete previous config files and render again? (y/n)"; then
+            say "[+] Exiting..."
+            keep_configs=true
+            exit 0
         fi
         rm -rf "${dir:?}"/*
     fi
@@ -175,8 +197,7 @@ check_sops () {
             return 1
         fi
     else
-        echo "[-] Invalid data format while checking for encryption."
-        exit 1
+        say "[-] Invalid data format while checking for encryption."
     fi
 }
 
@@ -187,8 +208,7 @@ sops_decrypt() {
 
     # Check if sops is installed
     if ! command -v sops &> /dev/null; then
-        echo "[-] Decryption failed, SOPS not installed."
-        exit 1
+        say "[-] Decryption failed, SOPS not installed."
     fi
 
     # Create a temporary file for the decrypted output
@@ -196,40 +216,27 @@ sops_decrypt() {
 
     # Decrypt file and write output to temporary file
     if ! sops --input-type "$filetype" --output-type "$filetype" -d "$file" > "$decrypted_temp_file"; then
-        echo "[-] Decryption failed."
-        exit 1
+        say "[-] Decryption failed."
     else
-        echo "[+] Decryption successful."
+        say "[+] Decryption successful."
         config_file="$decrypted_temp_file"
     fi
 }
 
-# Check if previous build files exists
+# Build images using nix-build command
 build_images() {
     local total_hosts
     total_hosts=$(echo "$hostnames" | wc -w)
 
     # Check if output directory exists and prompt user if necessary
     if [ -d "$output_dir" ] && [ "$(ls -A "$output_dir")" ]; then
-        if [ "$prompt" == true ]; then
-            read -r -p "[?] Delete previous images and rebuild? (y/n)" choice
-        else
-            choice="y"
-        fi
-        if [ "$choice" != "y" ]; then
-            echo "[+] Exiting..."
-            exit 1
+        if ! say "[?] Delete previous images and rebuild? (y/n)"; then
+            exit 0
         fi
         rm -rf "${output_dir:?}"/*
     else
-        if [ "$prompt" == true ]; then
-            read -r -p "[?] Proceed with building? (y/n)" choice
-        else
-            choice="y"
-        fi
-        if [ "$choice" != "y" ]; then
-            echo "[+] Exiting..."
-            exit 1
+        if ! say "[?] Proceed with building? (y/n)"; then
+            exit 0
         fi
     fi
 
@@ -240,60 +247,63 @@ build_images() {
     counter=0
 
     # Loop through the hosts and build the images
-    declare -A symlink_paths
     for host in $hostnames; do
         
         # Print host name
         (( counter++ ))
-        echo -e "\n[+] Building images for $host [$counter/$total_hosts]"
+        say "\n[+] Building images for $host [$counter/$total_hosts]"
         
         # Build images for $host using nix-build command
-        time nix-build \
-            -A pix.ipxe "$SCRIPT_DIR"/configs/nix_configs/hosts/"$host"/default.nix \
-            -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-unstable.zip \
-            -I home-manager=https://github.com/nix-community/home-manager/archive/master.tar.gz \
-            -o "$output_dir"/"$host" ;
-            #--show-trace ;
+        if "$verbose"; then
+            nix-build \
+                -A pix.ipxe "$config_dir"/"$host"/default.nix \
+                -I nixpkgs="$NIXPKGS_REPO" \
+                -I home-manager="$HM_REPO" \
+                -o "$output_dir"/"$host";
+        else
+            nix-build \
+                -A pix.ipxe "$config_dir"/"$host"/default.nix \
+                -I nixpkgs="$NIXPKGS_REPO" \
+                -I home-manager="$HM_REPO" \
+                -o "$output_dir"/"$host" >/dev/null 2>&1 ;
+        fi
 
         # Check if building images for $host was successful
         if [ $? -ne 0 ]; then
-            echo -e "[-] Build failed for $host"
-            if [ "$prompt" == true ]; then
-                read -r -p "[?] Continue? (y/n)" choice
-            fi
-            if [ "$choice" != "y" ]; then
+            say "[-] Build failed for $host"
+            if ! say "[?] Continue? (y/n)"; then
                 rm -rf "${output_dir:?}"/"$host"
-                echo "[+] Exiting..."
-                exit 1
+                say "[+] Exiting..."
+                exit 0
             fi
         else
-            echo "[+] Succesfully built images for $host"
+            say "[+] Succesfully built images for $host"
             
-            # Add the symlink paths to the array
-            symlink_paths[$host]=$(readlink -f "$output_dir"/"$host"/* | sort -u)
+            # Print the symlink target paths to the array
+            mapfile -t symlink_paths < <(readlink -f "$output_dir"/"$host"/* | sort -u)
+            say "\n[+] $host - result"
+            for path in "${symlink_paths[@]}"; do
+                if "$verbose"; then
+                    if [[ "${path}" == "${symlink_paths[-1]}" ]]; then
+                        say " └── ${path}"
+                    else
+                        say " ├── ${path}"
+                    fi
+                else
+                    echo "${path}"
+                fi
+            done
         fi
-    done
-
-    # Print the symlink paths (fancy)
-    for host in $(echo "${!symlink_paths[@]}" | tr ' ' '\n' | sort); do
-        echo "[+] $host - result"
-        # Split the string into an array
-        IFS=$'\n' read -rd '' -a paths <<< "${symlink_paths[$host]}"
-        for path in "${paths[@]}"; do
-            if [[ "${path}" == "${paths[-1]}" ]]; then
-                echo " └── ${path}"
-            else
-                echo " ├── ${path}"
-            fi
-        done
     done
 
     # End the timer
     secs=$SECONDS
 
     # Print the message with the time in the desired format
-    hrs=$(( secs/3600 )); mins=$(( (secs-hrs*3600)/60 )); secs=$(( secs-hrs*3600-mins*60 ))
-    printf "\n[+] Build(s) completed in: %02d:%02d:%02d\n" $hrs $mins $secs
+    hrs=$(printf "%02d" $((secs/3600)))
+    mins=$(printf "%02d" $(((secs/60)%60)))
+    secs=$(printf "%02d" $((secs%60)))
+    say "\n[+] Build(s) completed in: $hrs:$mins:$secs"
 }
 
 # Clean up
@@ -307,17 +317,12 @@ cleanup() {
     done
 
     # Remove configuration files
-    if [ "$keep_configs" == false ]; then
-        if [ "$prompt" == true ]; then
-            read -r -p "[?] Delete rendered config files? (y/n)" choice
-        else
-            choice="y"
+    if [ -d "$config_dir" ] && [ "$(ls -A "$config_dir")" ] && [ "$keep_configs" == false ]; then
+        if ! say "[?] Delete rendered config files? (y/n)"; then
+            say "[+] Exiting..."
+            exit 0
         fi
-        if [ "$choice" != "y" ]; then
-            echo "[+] Exiting..."
-            exit 1
-        fi
-        rm -rf "$SCRIPT_DIR"/configs/nix_configs/hosts/*
+        rm -rf "${config_dir:?}"/*
     fi
 }
 
@@ -337,15 +342,13 @@ main() {
         # Get stdin and save to temporary file (sets config_file)
         get_stdin  
         if [[ ${prompt} == true ]]; then
-            echo "[-] Prompt needs to be disabled when data is piped to the script."
-            exit 1
+            say "[-] Prompt needs to be disabled when data is piped to the script."
         fi
     fi
     
     # Check if config_file is set and exists
-    if [[ ! -n "$config_file" && ! -e "$config_file" ]]; then
-        echo "[-] No given configuration data found."
-        exit 1
+    if [[ -z "$config_file" && ! -e "$config_file" ]]; then
+        say "[-] No given configuration data found."
     fi
 
     # Get filetype (yaml or json)
@@ -355,13 +358,13 @@ main() {
     get_hostnames "$config_file" "$filetype"
     
     # Create required directories if they don't exist
-    directories=( "$output_dir" "$SCRIPT_DIR/configs/nix_configs/hosts" )
+    directories=( "$output_dir" "$config_dir" )
     for dir in "${directories[@]}"; do
         mkdir -p "$dir"
     done
 
     # Check if previous config files exist
-    check_prev_config "$SCRIPT_DIR/configs/nix_configs/hosts"
+    check_prev_config "$config_dir"
 
     # Decrypt if file is encrypted with SOPS
     if check_sops "$config_file" "$filetype"; then
@@ -370,9 +373,21 @@ main() {
 
     # Render the Nix config files using the python script
     if ! $python_cmd "$SCRIPT_DIR/configs/render_configs.py" "$config_file"; then
-        echo "[-] Rendering failed."
-        exit 1
+        say "[-] Rendering failed."
     fi
+
+    # Print the config paths
+    for host in $hostnames; do
+        mapfile -t subdir_files < <(find "$config_dir"/"$host" -type f -printf '%P\n')
+        say "\n[+] $host - config"
+        for path in "${subdir_files[@]}"; do
+            if [[ "${path}" == "${subdir_files[-1]}" ]]; then
+                say " └── ${path}"
+            else
+                say " ├── ${path}"
+            fi
+        done
+    done
 
     # Check if previous build files exists
     build_images
