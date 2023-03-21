6dfb95be61a345c6bd3ead8c06d77afb616f8155
tupakkatapa <jeskar95@outlook.com>
2023-03-21 13:58:58 +0200

    Added pkgs/default.nix (example)

---
+++ configs/nix_configs/pkgs/default.nix
@@ -0,0 +1,7 @@
+# Nixobolus - Automated creation of bootable NixOS images
+# https://github.com/ponkila/Nixobolus
+
+{ pkgs ? import <nixpkgs> { } }:
+{
+    # example = pkgs.callPackage ./example { };
+}
