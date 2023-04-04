ccc32479918e5549ca690e1ef230738cc8ac76a3
tupakkatapa <jeskar95@outlook.com>
2023-04-04 12:30:51 +0300

    Update copy_results.sh for flake combability

--- scripts/copy_result.sh
+++ scripts/copy_result.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+# Nixobolus - Automated creation of bootable NixOS images
+# https://github.com/ponkila/Nixobolus
+# Example: ./scripts/copy_result.sh <hostname> /mnt/sftp/netboot
 
 PARENT_DIR=$( cd -- "$( dirname -- "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" )" &> /dev/null && pwd )
-TARGET_DIR="$1"
+HOST="$1"
+TARGET_DIR="$2"
 
 # create target directory if it doesn't exist
 mkdir -p "$TARGET_DIR"
 
 # define an array with the files to copy
-files=( "$PARENT_DIR/result/bzImage"
-        "$PARENT_DIR/result/initrd"
-        "$PARENT_DIR/result/netboot.ipxe"
+files=( "bzImage"
+        "initrd"
+        "netboot.ipxe"
 )
 
 # loop through the array and copy each file to target directory
 for file in "${files[@]}"
 do
-    cp -f "$file" "$TARGET_DIR"
+    cp -f "$PARENT_DIR/result/$HOST/$file" "$TARGET_DIR"
 done
