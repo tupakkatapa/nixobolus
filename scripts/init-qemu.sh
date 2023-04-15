e8cff019c6b84a388b1b02a647c57befda977bd4
tupakkatapa <jeskar95@outlook.com>
2023-04-15 13:59:24 +0300

    init-qemu.sh: refactor

--- scripts/init-qemu.sh
+++ scripts/init-qemu.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# yay -S qemu-desktop libvirt virt-install
+# deps: qemu-desktop libvirt virt-install
+# desc: a simple script for converting kexecTree to qcow2
+# usage: init-qemu.sh <hostname>
 
 # ERROR Operation not supported: Cannot use direct socket mode if no URI is set
-# uri_default = "qemu:///session" >> /etc/libvirt/libvirt.conf
+# sudo sh -c 'echo "uri_default = \"qemu:///system\"" >> /etc/libvirt/libvirt.conf'
 #
 # ERROR Failed to connect socket to '/var/run/libvirt/virtqemud-sock': No such file or directory
-# systemctl restart libvirtd
-# systemctl enable libvirtd
+# systemctl restart libvirtd && systemctl enable libvirtd
 #
 # ERROR Cannot get interface MTU on 'virbr0': No such device
 # sudo virsh net-create /etc/libvirt/qemu/networks/default.xml
@@ -15,25 +16,28 @@
 HOSTNAME="$1"
 
 # Paths
+#SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PARENT_DIR=$( cd -- "$( dirname -- "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" )" &> /dev/null && pwd )
 HOST_DIR="$PARENT_DIR/result/$HOSTNAME/"
 IMAGE_DIR="/var/lib/libvirt/images"
+#IMAGE_DIR="/mnt/data/da/var/lib/libvirt/images"
 # if not default folder /var/lib/libvirt/images, do selinux fix:
 # sudo semanage fcontext -a -t svirt_image_t "/mnt/data/images(/.*)?"
 # sudo restorecon -vR /mnt/data/images
 
 # Server random number
 randomNumber=$((1 + $RANDOM % 9999))
-#echo "Random number $randomNumber"
+echo "Random number $randomNumber"
 
 # Define variables
-VM_NAME="test"
+VM_NAME="$HOSTNAME"
 RAM=2048 # MB
 VCPUS=2
 DISK_SIZE=10 # G
+
+QCOW2_FILE="$IMAGE_DIR/$VM_NAME-$randomNumber.qcow2"
 INITRD_FILE=$(readlink -f "$HOST_DIR/initrd")
 BZIMAGE_FILE=$(readlink -f "$HOST_DIR/bzImage")
-QCOW2_FILE="$IMAGE_DIR/$VM_NAME-$randomNumber.qcow2"
 KERNEL_ARGS=$(sed -n '/--command-line/p' $HOST_DIR/kexec-boot | cut -d\" -f2)
 
 # ERROR Guest name 'foobar' is already in use.
