d69664bd55c796b5541ef9ed98186cffa1bf441d
tupakkatapa <jeskar95@outlook.com>
2023-04-28 13:37:16 +0000

    erigon: refactor with let

--- modules/eth/erigon.nix
+++ modules/eth/erigon.nix
@@ -1,54 +1,53 @@
 { pkgs, config, lib, ... }:
+let
+  options = {
+    enable = false;
+    user = "";
+    endpoint = "";
+    datadir = "";
+  };
+in
 {
-  erigon = rec {
+  config = lib.mkIf options.enable {
+    # package
+    environment.systemPackages = with pkgs; [
+      erigon
+    ];
 
-    options = {
-      enable = false;
-      user = "";
-      endpoint = "";
-      datadir = "";
+    # service
+    systemd.services.erigon = {
+      enable = true;
+
+      description = "execution, mainnet";
+      requires = [ "wg0.service" ];
+      after = [ "wg0.service" "lighthouse.service" ];
+
+      serviceConfig = {
+        Restart = "always";
+        RestartSec = "5s";
+        User = options.user;
+        Group = options.user;
+        Type = "simple";
+      };
+
+      script = ''${pkgs.erigon}/bin/erigon \
+        --datadir=${options.datadir} \
+        --chain mainnet \
+        --authrpc.vhosts="*" \
+        --authrpc.addr ${options.endpoint} \
+        --authrpc.jwtsecret=${options.datadir}/jwt.hex \
+        --metrics \
+        --externalcl
+      '';
+
+      wantedBy = [ "multi-user.target" ];
     };
 
-    config = lib.mkIf options.enable {
-      # package
-      environment.systemPackages = with pkgs; [
-        erigon
-      ];
-
-      # service
-      systemd.services.erigon = {
-        enable = true;
-
-        description = "execution, mainnet";
-        requires = [ "wg0.service" ];
-        after = [ "wg0.service" "lighthouse.service" ];
-
-        serviceConfig = {
-          Restart = "always";
-          RestartSec = "5s";
-          User = options.user;
-          Group = options.user;
-          Type = "simple";
-        };
-
-        script = ''${pkgs.erigon}/bin/erigon \
-          --datadir=${options.datadir} \
-          --chain mainnet \
-          --authrpc.vhosts="*" \
-          --authrpc.addr ${options.endpoint} \
-          --authrpc.jwtsecret=${options.datadir}/jwt.hex \
-          --metrics \
-          --externalcl
-        '';
-
-        wantedBy = [ "multi-user.target" ];
-      };
-
-      # firewall
-      networking.firewall = {
-        allowedTCPPorts = [ 30303 30304 42069 ];
-        allowedUDPPorts = [ 30303 30304 42069 ];
-      };
+    # firewall
+    networking.firewall = {
+      allowedTCPPorts = [ 30303 30304 42069 ];
+      allowedUDPPorts = [ 30303 30304 42069 ];
     };
   };
 }
+
