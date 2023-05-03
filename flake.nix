fee21fb2cf216636a78fc850e76f1fcb42eda142
tupakkatapa <jeskar95@outlook.com>
2023-05-03 08:32:36 +0000

    flake: refactor

--- flake.nix
+++ flake.nix
@@ -321,6 +321,27 @@
             };
           })
 
+          #################################################################### WIREGUARD (no options)
+          (mkIf true {
+            systemd.services.wg0 = {
+              enable = true;
+
+              description = "wireguard interface for cross-node communication";
+              requires = [ "network-online.target" ];
+              after = [ "network-online.target" ];
+
+              serviceConfig = {
+                Type = "oneshot";
+              };
+
+              script = ''${nixpkgs.wireguard-tools}/bin/wg-quick \
+              up /run/user/1000/wireguard/wg0.conf
+              '';
+
+              wantedBy = [ "multi-user.target" ];
+            };
+          })
+
           #################################################################### ERIGON
           (mkIf (options.erigon.enable) {
             # package
@@ -387,19 +408,19 @@
               };
 
               script = ''${pkgs.mev-boost}/bin/mev-boost \
-            -mainnet \
-            -relay-check \
-            -relays ${lib.concatStringsSep "," [
-              "https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net"
-              "https://0xad0a8bb54565c2211cee576363f3a347089d2f07cf72679d16911d740262694cadb62d7fd7483f27afd714ca0f1b9118@bloxroute.ethical.blxrbdn.com"
-              "https://0x9000009807ed12c1f08bf4e81c6da3ba8e3fc3d953898ce0102433094e5f22f21102ec057841fcb81978ed1ea0fa8246@builder-relay-mainnet.blocknative.com"
-              "https://0xb0b07cd0abef743db4260b0ed50619cf6ad4d82064cb4fbec9d3ec530f7c5e6793d9f286c4e082c0244ffb9f2658fe88@bloxroute.regulated.blxrbdn.com"
-              "https://0x8b5d2e73e2a3a55c6c87b8b6eb92e0149a125c852751db1422fa951e42a09b82c142c3ea98d0d9930b056a3bc9896b8f@bloxroute.max-profit.blxrbdn.com"
-              "https://0x98650451ba02064f7b000f5768cf0cf4d4e492317d82871bdc87ef841a0743f69f0f1eea11168503240ac35d101c9135@mainnet-relay.securerpc.com"
-              "https://0x84e78cb2ad883861c9eeeb7d1b22a8e02332637448f84144e245d20dff1eb97d7abdde96d4e7f80934e5554e11915c56@relayooor.wtf"
-            ]} \
-            -addr 0.0.0.0:18550
-        '';
+                -mainnet \
+                -relay-check \
+                -relays ${lib.concatStringsSep "," [
+                  "https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net"
+                  "https://0xad0a8bb54565c2211cee576363f3a347089d2f07cf72679d16911d740262694cadb62d7fd7483f27afd714ca0f1b9118@bloxroute.ethical.blxrbdn.com"
+                  "https://0x9000009807ed12c1f08bf4e81c6da3ba8e3fc3d953898ce0102433094e5f22f21102ec057841fcb81978ed1ea0fa8246@builder-relay-mainnet.blocknative.com"
+                  "https://0xb0b07cd0abef743db4260b0ed50619cf6ad4d82064cb4fbec9d3ec530f7c5e6793d9f286c4e082c0244ffb9f2658fe88@bloxroute.regulated.blxrbdn.com"
+                  "https://0x8b5d2e73e2a3a55c6c87b8b6eb92e0149a125c852751db1422fa951e42a09b82c142c3ea98d0d9930b056a3bc9896b8f@bloxroute.max-profit.blxrbdn.com"
+                  "https://0x98650451ba02064f7b000f5768cf0cf4d4e492317d82871bdc87ef841a0743f69f0f1eea11168503240ac35d101c9135@mainnet-relay.securerpc.com"
+                  "https://0x84e78cb2ad883861c9eeeb7d1b22a8e02332637448f84144e245d20dff1eb97d7abdde96d4e7f80934e5554e11915c56@relayooor.wtf"
+                ]} \
+                -addr 0.0.0.0:18550
+              '';
 
               wantedBy = [ "multi-user.target" ];
             };
@@ -451,27 +472,6 @@
               interfaces."wg0".allowedTCPPorts = [
                 5052 # lighthouse
               ];
-            };
-          })
-
-          #################################################################### WIREGUARD (no options)
-          (mkIf true {
-            systemd.services.wg0 = {
-              enable = true;
-
-              description = "wireguard interface for cross-node communication";
-              requires = [ "network-online.target" ];
-              after = [ "network-online.target" ];
-
-              serviceConfig = {
-                Type = "oneshot";
-              };
-
-              script = ''${nixpkgs.wireguard-tools}/bin/wg-quick \
-              up /run/user/1000/wireguard/wg0.conf
-              '';
-
-              wantedBy = [ "multi-user.target" ];
             };
           })
         ];
