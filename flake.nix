0d386fbabbaf78426e3d88c2a64ae4ac995e3326
tupakkatapa <jeskar95@outlook.com>
2023-06-06 23:54:40 +0000

    dump 6

--- flake.nix
+++ flake.nix
@@ -24,8 +24,6 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     mission-control.url = "github:Platonic-Systems/mission-control";
-    #nixobolus.url = "github:ponkila/nixobolus";
-    nixobolus.url = "path:./modules/homestakeros";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-22.11";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
@@ -39,7 +37,6 @@
     , ethereum-nix
     , flake-parts
     , home-manager
-    , nixobolus
     , nixpkgs
     , nixpkgs-stable
     , sops-nix
@@ -88,6 +85,94 @@
         let
           inherit (self) outputs;
 
+          homestakeros_options = with nixpkgs.lib; {
+            localization = {
+              hostname = mkOption {
+                type = types.str;
+                default = "homestaker";
+              };
+              timezone = mkOption {
+                type = types.str;
+                default = "Europe/Helsinki";
+              };
+            };
+
+            # mounts = mkOption {
+            #   type = types.attrsOf types.string;
+            # };
+
+            ssh = {
+              privateKeyPath = mkOption {
+                type = types.path;
+                default = "/var/mnt/secrets/ssh/id_ed25519";
+              };
+            };
+
+            user = {
+              authorizedKeys = mkOption {
+                type = types.listOf types.singleLineStr;
+                default = [ ];
+              };
+            };
+
+            erigon = {
+              enable = mkOption {
+                type = types.bool;
+                default = false;
+              };
+              endpoint = mkOption {
+                type = types.str;
+                default = "http://127.0.0.1:8551";
+              };
+              datadir = mkOption {
+                type = types.str;
+                default = "/var/mnt/erigon";
+              };
+            };
+
+            lighthouse = {
+              enable = mkOption {
+                type = types.bool;
+                default = false;
+              };
+              endpoint = mkOption {
+                type = types.str;
+                default = "http://127.0.0.1:5052";
+              };
+              exec.endpoint = mkOption {
+                type = types.str;
+                default = "http://127.0.0.1:8551";
+              };
+              slasher = {
+                enable = mkOption {
+                  type = types.bool;
+                  default = false;
+                };
+                history-length = mkOption {
+                  type = types.int;
+                  default = 4096;
+                };
+                max-db-size = mkOption {
+                  type = types.int;
+                  default = 256;
+                };
+              };
+              mev-boost = {
+                enable = mkOption {
+                  type = types.bool;
+                  default = false;
+                };
+                endpoint = mkOption {
+                  type = types.str;
+                  default = "http://127.0.0.1:18550";
+                };
+              };
+              datadir = mkOption {
+                type = types.str;
+              };
+            };
+          };
+
           homestakeros = {
             system = "x86_64-linux";
             specialArgs = { inherit inputs outputs; };
@@ -95,7 +180,7 @@
               ./system
               ./system/ramdisk.nix
               ./system/formats/netboot-kexec.nix
-              #nixobolus.nixosModules.homestakeros
+              self.nixosModules.homestakeros
               home-manager.nixosModules.home-manager
               disko.nixosModules.disko
               {
@@ -118,7 +203,7 @@
           exports = nixpkgs.lib.attrsets.mapAttrsRecursiveCond
             (v: ! nixpkgs.lib.options.isOption v)
             (k: v: v.type.name)
-            nixosModules.homestakeros.options;
+            homestakeros_options;
 
           overlays = import ./overlays { inherit inputs; };
 
@@ -126,92 +211,267 @@
             "homestakeros" = nixosSystem (getAttrs [ "system" "specialArgs" "modules" ] homestakeros);
           } // (with nixpkgs-stable.lib; { });
 
-          nixosModules.homestakeros = with nixpkgs.lib; {
-            imports = [ ./modules/homestakeros ];
-            options = {
-              localization = {
-                hostname = mkOption {
-                  type = types.str;
-                  default = "homestaker";
-                };
-                timezone = mkOption {
-                  type = types.str;
-                  default = "Europe/Helsinki";
-                };
-              };
+          # usage: https://github.com/ponkila/homestaking-infra/commit/574382212cf817dbb75657e9fef9cdb223e9823b
+          nixosModules.homestakeros = { config, lib, pkgs, ... }: with nixpkgs.lib;
+            let
+              cfg = config.homestakeros;
+            in
+            {
+              options.homestakeros = homestakeros_options;
 
-              mounts = mkOption {
-                type = types.attrsOf types.string;
-              };
+              config = mkMerge [
+                ################################################################### LOCALIZATION
+                (mkIf true {
+                  networking.hostName = cfg.localization.hostname;
+                  time.timeZone = cfg.localization.timezone;
+                })
 
-              ssh = {
-                privateKeyPath = mkOption {
-                  type = types.path;
-                  default = "/var/mnt/secrets/ssh/id_ed25519";
-                };
-              };
+                #################################################################### MOUNTS
+                # (mkIf true {
+                #   systemd.mounts = builtins.listToAttrs (map
+                #     (mount: {
+                #       enable = mount.enable or true;
+                #       description = mount.description or "Unnamed mount point";
+                #       what = mount.what;
+                #       where = mount.where;
+                #       type = mount.type or "ext4";
+                #       options = mount.options or "defaults";
+                #       before = lib.mkDefault mount.before;
+                #       wantedBy = mount.wantedBy or [ "multi-user.target" ];
+                #     })
+                #     (lib.mkDefault cfg.mounts or []));
+                # })
 
-              user = {
-                authorizedKeys = mkOption {
-                  type = types.listOf types.singleLineStr;
-                  default = [ ];
-                };
-              };
-
-              erigon = {
-                enable = mkOption {
-                  type = types.bool;
-                  default = false;
-                };
-                endpoint = mkOption {
-                  type = types.str;
-                  default = "http://${infra.ip}:8551";
-                };
-                datadir = mkOption {
-                  type = types.str;
-                  default = "/var/mnt/erigon";
-                };
-              };
-
-              lighthouse = {
-                enable = mkOption {
-                  type = types.bool;
-                  default = false;
-                };
-                endpoint = mkOption {
-                  type = types.str;
-                  default = "http://${infra.ip}:5052";
-                };
-                exec.endpoint = mkOption {
-                  type = types.str;
-                  default = "http://${infra.ip}:8551";
-                };
-                slasher = {
-                  enable = mkOption {
-                    type = types.bool;
-                    default = false;
+                #################################################################### SSH (system level)
+                (mkIf true {
+                  services.openssh = {
+                    enable = true;
+                    settings.PasswordAuthentication = false;
+                    hostKeys = [{
+                      path = cfg.ssh.privateKeyPath;
+                      type = "ed25519";
+                    }];
                   };
-                  history-length = mkOption {
-                    type = types.int;
-                    default = 4096;
+                })
+
+                #################################################################### USER (core)
+                (mkIf true {
+                  services.getty.autologinUser = "core";
+                  users.users.core = {
+                    isNormalUser = true;
+                    group = "core";
+                    extraGroups = [ "wheel" ];
+                    openssh.authorizedKeys.keys = cfg.user.authorizedKeys;
+                    shell = pkgs.fish;
                   };
-                  max-db-size = mkOption {
-                    type = types.int;
-                    default = 256;
+                  users.groups.core = { };
+                  environment.shells = [ pkgs.fish ];
+                  programs.fish.enable = true;
+
+                  home-manager.users.root.home.stateVersion = config.home-manager.users.core.home.stateVersion;
+                  home-manager.users.core = { pkgs, ... }: {
+
+                    home.packages = with pkgs; [
+                      file
+                      tree
+                      bind # nslookup
+                    ];
+
+                    programs = {
+                      tmux.enable = true;
+                      htop.enable = true;
+                      vim.enable = true;
+                      git.enable = true;
+                      fish.enable = true;
+                      fish.loginShellInit = "fish_add_path --move --prepend --path $HOME/.nix-profile/bin /run/wrappers/bin /etc/profiles/per-user/$USER/bin /run/current-system/sw/bin /nix/var/nix/profiles/default/bin";
+
+                      home-manager.enable = true;
+                    };
+
+                    home.stateVersion = "23.05";
                   };
-                };
-                mev-boost = {
-                  endpoint = mkOption {
-                    type = types.str;
-                    default = "http://${infra.ip}:18550";
+                })
+
+                #################################################################### WIREGUARD (no options)
+                (mkIf true {
+                  # home-manager.users.core = { pkgs, ... }: {
+                  #   sops = {
+                  #     defaultSopsFile = ./secrets/default.yaml;
+                  #     secrets."wireguard/wg0" = {
+                  #       path = "%r/wireguard/wg0.conf";
+                  #     };
+                  #     age.sshKeyPaths = [ cfg.ssh.privateKeyPath ];
+                  #   };
+                  # };
+                  systemd.services.wg0 = {
+                    enable = true;
+
+                    description = "wireguard interface for cross-node communication";
+                    requires = [ "network-online.target" ];
+                    after = [ "network-online.target" ];
+
+                    serviceConfig = {
+                      Type = "oneshot";
+                    };
+
+                    script = ''${pkgs.wireguard-tools}/bin/wg-quick \
+                      up /run/user/1000/wireguard/wg0.conf
+                    '';
+
+                    wantedBy = [ "multi-user.target" ];
                   };
-                };
-                datadir = mkOption {
-                  type = types.str;
-                };
-              };
+                })
+
+                #################################################################### ERIGON
+                (mkIf (cfg.erigon.enable) {
+                  # package
+                  environment.systemPackages = [
+                    pkgs.erigon
+                  ];
+
+                  # service
+                  systemd.user.services.erigon =
+                    let
+                      # split endpoint to address and port
+                      endpointRegex = "(https?://)?([^:/]+):([0-9]+)(/.*)?$";
+                      endpointMatch = builtins.match endpointRegex cfg.erigon.endpoint;
+                      endpoint = {
+                        addr = builtins.elemAt endpointMatch 1;
+                        port = builtins.elemAt endpointMatch 2;
+                      };
+                    in
+                    {
+                      enable = true;
+
+                      description = "execution, mainnet";
+                      requires = [ "wg0.service" ];
+                      after = [ "wg0.service" "lighthouse.service" ];
+
+                      serviceConfig = {
+                        Restart = "always";
+                        RestartSec = "5s";
+                        Type = "simple";
+                      };
+
+                      script = ''${pkgs.erigon}/bin/erigon \
+                      --datadir=${cfg.datadir} \
+                      --chain mainnet \
+                      --authrpc.vhosts="*" \
+                      --authrpc.port ${endpoint.port} \
+                      --authrpc.addr ${endpoint.addr} \
+                      --authrpc.jwtsecret=%r/jwt.hex \
+                      --metrics \
+                      --externalcl
+                    '';
+
+                      wantedBy = [ "multi-user.target" ];
+                    };
+
+                  # firewall
+                  networking.firewall = {
+                    allowedTCPPorts = [ 30303 30304 42069 ];
+                    allowedUDPPorts = [ 30303 30304 42069 ];
+                  };
+                })
+
+                #################################################################### MEV-BOOST
+                (mkIf (cfg.lighthouse.mev-boost.enable) {
+                  # service
+                  systemd.user.services.mev-boost = {
+                    enable = true;
+
+                    description = "MEV-boost allows proof-of-stake Ethereum consensus clients to outsource block construction";
+                    requires = [ "wg0.service" ];
+                    after = [ "wg0.service" ];
+
+                    serviceConfig = {
+                      Restart = "always";
+                      RestartSec = "5s";
+                      Type = "simple";
+                    };
+
+                    script = ''${pkgs.mev-boost}/bin/mev-boost \
+                      -mainnet \
+                      -relay-check \
+                      -relays ${lib.concatStringsSep "," [
+                        "https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net"
+                        "https://0xad0a8bb54565c2211cee576363f3a347089d2f07cf72679d16911d740262694cadb62d7fd7483f27afd714ca0f1b9118@bloxroute.ethical.blxrbdn.com"
+                        "https://0x9000009807ed12c1f08bf4e81c6da3ba8e3fc3d953898ce0102433094e5f22f21102ec057841fcb81978ed1ea0fa8246@builder-relay-mainnet.blocknative.com"
+                        "https://0xb0b07cd0abef743db4260b0ed50619cf6ad4d82064cb4fbec9d3ec530f7c5e6793d9f286c4e082c0244ffb9f2658fe88@bloxroute.regulated.blxrbdn.com"
+                        "https://0x8b5d2e73e2a3a55c6c87b8b6eb92e0149a125c852751db1422fa951e42a09b82c142c3ea98d0d9930b056a3bc9896b8f@bloxroute.max-profit.blxrbdn.com"
+                        "https://0x98650451ba02064f7b000f5768cf0cf4d4e492317d82871bdc87ef841a0743f69f0f1eea11168503240ac35d101c9135@mainnet-relay.securerpc.com"
+                        "https://0x84e78cb2ad883861c9eeeb7d1b22a8e02332637448f84144e245d20dff1eb97d7abdde96d4e7f80934e5554e11915c56@relayooor.wtf"
+                      ]} \
+                      -addr 0.0.0.0:18550
+                    '';
+
+                    wantedBy = [ "multi-user.target" ];
+                  };
+                })
+
+                #################################################################### LIGHTHOUSE
+                (mkIf (cfg.lighthouse.enable) {
+                  # package
+                  environment.systemPackages = with pkgs; [
+                    lighthouse
+                  ];
+
+                  # service
+                  systemd.user.services.lighthouse =
+                    let
+                      # split endpoint to address and port
+                      endpointRegex = "(https?://)?([^:/]+):([0-9]+)(/.*)?$";
+                      endpointMatch = builtins.match endpointRegex cfg.erigon.endpoint;
+                      endpoint = {
+                        addr = builtins.elemAt endpointMatch 1;
+                        port = builtins.elemAt endpointMatch 2;
+                      };
+                    in
+                    {
+                      enable = true;
+
+                      description = "beacon, mainnet";
+                      requires = [ "wg0.service" ];
+                      after = [ "wg0.service" "mev-boost.service" ];
+
+                      serviceConfig = {
+                        Restart = "always";
+                        RestartSec = "5s";
+                        Type = "simple";
+                      };
+
+                      script = ''${pkgs.lighthouse}/bin/lighthouse bn \
+                      --datadir ${cfg.datadir} \
+                      --network mainnet \
+                      --http --http-address ${endpoint.addr} \
+                      --http-port ${endpoint.port} \
+                      --http-allow-origin "*" \
+                      --execution-endpoint ${cfg.exec.endpoint} \
+                      --execution-jwt %r/jwt.hex \
+                      --builder ${cfg.mev-boost.endpoint} \
+                      --prune-payloads false \
+                      --metrics \
+                      ${if cfg.slasher.enable then
+                        "--slasher "
+                        + " --slasher-history-length " + (toString cfg.slasher.history-length)
+                        + " --slasher-max-db-size " + (toString cfg.slasher.max-db-size)
+                      else "" }
+                    '';
+                      wantedBy = [ "multi-user.target" ];
+                    };
+
+                  # firewall
+                  networking.firewall = {
+                    allowedTCPPorts = [ 9000 ];
+                    allowedUDPPorts = [ 9000 ];
+                    interfaces."wg0".allowedTCPPorts = [
+                      5052 # TODO: use 'lighthouse.endpoint.port' here by converting it to u16
+                    ];
+                  };
+                })
+              ];
             };
-          };
         };
     };
 }
+
