66daf59c17892a1fd15b4e12a808d8e9b635bba7
tupakkatapa <jeskar95@outlook.com>
2023-06-10 06:45:48 +0000

    overlays: erigon and lighthouse version

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,8 +5,31 @@
 
   # This one contains whatever you want to overlay
   modifications = final: prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
+    lighthouse = prev.lighthouse.overrideAttrs (oldAttrs: rec {
+      # Version override
+      pname = "lighthouse";
+      version = "4.1.0";
+      src = final.fetchFromGitHub {
+        owner = "sigp";
+        repo = "lighthouse";
+        rev = "v${version}";
+        hash = "sha256-10DpoG9MS6jIod0towzIsmyyakfiT62NIJBKxqsgsK0=";
+      };
+      # Enables aggressive optimisations including full LTO
+      PROFILE = "maxperf";
+    });
+    erigon = prev.lighthouse.overrideAttrs (oldAttrs: rec {
+      # Version override
+      pname = "erigon";
+      version = "2.42.0";
+      src = final.fetchFromGitHub {
+        owner = "ledgerwatch";
+        repo = pname;
+        rev = "v${version}";
+        sha256 = "sha256-M2u8/WKo1yZu27KjTJhJFqycCxCopJqtVQpIs9inswI=";
+        fetchSubmodules = true;
+      };
+      vendorSha256 = "sha256-Vyurf4wSN4zSDjcH8FC+OOiviiSjRVF4RId/eqFDd+c=";
+    });
   };
 }
