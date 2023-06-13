17a503bc31195b2818b6745348a60f26c9186e83
tupakkatapa <jeskar95@outlook.com>
2023-06-13 10:21:50 +0000

    Revert "overlays: erigon and lighthouse version"
    This reverts commit 66daf59c17892a1fd15b4e12a808d8e9b635bba7.

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,32 +5,8 @@
 
   # This one contains whatever you want to overlay
   modifications = final: prev: {
-    lighthouse = prev.lighthouse.overrideAttrs (oldAttrs: rec {
-      # Version override
-      pname = "lighthouse";
-      version = "4.0.2-rc.0";
-      src = final.fetchFromGitHub {
-        owner = "sigp";
-        repo = pname;
-        rev = "v${version}";
-        hash = "sha256-10DpoG9MS6jIod0towzIsmyyakfiT62NIJBKxqsgsK0=";
-      };
-      cargoSha256 = "sha256-KlTQF1iL2PYAk+nmQIm72guy2PxGkN/YzhgCNv1FZGM=";
-      # Enables aggressive optimisations including full LTO
-      PROFILE = "maxperf";
-    });
-    erigon = prev.erigon.overrideAttrs (oldAttrs: rec {
-      # Version override
-      pname = "erigon";
-      version = "2.42.0";
-      src = final.fetchFromGitHub {
-        owner = "ledgerwatch";
-        repo = pname;
-        rev = "v${version}";
-        sha256 = "sha256-M2u8/WKo1yZu27KjTJhJFqycCxCopJqtVQpIs9inswI=";
-        fetchSubmodules = true;
-      };
-      vendorSha256 = "sha256-Vyurf4wSN4zSDjcH8FC+OOiviiSjRVF4RId/eqFDd+c=";
-    });
+    # example = prev.example.overrideAttrs (oldAttrs: rec {
+    # ...
+    # });
   };
 }
